3cd47be68b690505010acfc2f93ec81ab8caf267
dexyel <39320381+dexyel@users.noreply.github.com>
2023-01-29 14:09:43 +0100

    alpha

---
+++ Scripts/getData.mjs
@@ -0,0 +1,51 @@
+//#region FIREBASE INIT
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
+import { getFirestore, doc, getDoc } from 'https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js';
+
+const firebaseConfig = {
+apiKey: "AIzaSyCo_urqG3I5MEb1eBCmHOJm15ufWzW5Vbo",
+authDomain: "odos-375812.firebaseapp.com",
+projectId: "odos-375812",
+storageBucket: "odos-375812.appspot.com",
+messagingSenderId: "377747953042",
+appId: "1:377747953042:web:61fdf5c517003581ce9d4b",
+measurementId: "G-R8SCX24SWJ"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+//#endregion
+
+const date = new Date();
+let currentDate = `${date.getDate()}${date.getMonth()+1}${date.getFullYear()}`;
+
+console.log(currentDate);
+
+const docRef = doc(db, 'song-collection', `${currentDate}`);
+const docSnap = await getDoc(docRef);
+
+if (docSnap.exists()) {
+    console.log('data:', docSnap.data());
+}
+else {
+    console.log('nope');
+}
+
+const todayArtist = docSnap.data().artist;
+const todayTitle = docSnap.data().title;
+const todayID = docSnap.data().id;
+const todayURL = docSnap.data().url;
+const todayBackground = docSnap.data().background;
+
+const geniusOptions = {
+    apiKey: 'DeMGzjuAqIRJIB5Oslo6A3Zh1OTiDLekoOIgQKljcRH7EJaDEsy7XXEdl6CxrMaS',
+    title: todayTitle,
+    artist: todayArtist,
+    optimizeQuery: true
+};
+
+localStorage.setItem('artist', todayArtist);
+localStorage.setItem('title', todayTitle);
+localStorage.setItem('url', todayURL);
+localStorage.setItem('id', todayID);
+localStorage.setItem('bg', todayBackground);
---
+++ Scripts/main.js
@@ -0,0 +1,98 @@
+//#region VARIABLES
+const leftDivs = document.querySelectorAll('#left-container div');
+const songCover = document.getElementById('song-cover');
+const songVideo = document.getElementById('song-video');
+const playButton = document.getElementById('play-button');
+const iframe = document.getElementById("embed-video");
+const artistName = document.getElementById('artist-name');
+const songTitle = document.getElementById('song-title');
+const lyricsDiv = document.getElementById('lyrics');
+
+let isPlaying = false;
+let player;
+//#endregion
+
+//#region LISTENERS
+playButton.addEventListener('click', handlePlayPause);
+
+leftDivs.forEach((div) => {
+    div.addEventListener('click', (e) => switchLeftView(e));
+});
+//#endregion
+
+//#region FUNCTIONS
+function onYouTubeIframeAPIReady() {
+    let artist = localStorage.getItem('artist');
+    let title = localStorage.getItem('title');
+    let id = localStorage.getItem('id');
+    let url = localStorage.getItem('url');
+    let bg = localStorage.getItem('bg');
+
+    player = new YT.Player(iframe, {
+        height: '100%',
+        width: '100%',
+        videoId: id,
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+
+    let img = document.createElement('img');
+    img.src = url;
+    songCover.appendChild(img);
+
+    artistName.textContent = artist;
+    songTitle.textContent = title;
+
+    let body = document.querySelector('body');
+    let background = document.createElement('img');
+    background.classList.add('background');
+    background.src = bg;
+    body.insertBefore(background, document.querySelector('#main-container'));
+}
+
+function onPlayerReady(event) {
+    // event.target.playVideo();
+    console.log("ready");
+}
+
+function onPlayerStateChange() {
+    isPlaying = !isPlaying;
+
+    updateButton();
+}
+
+function handlePlayPause() {
+    if (isPlaying) {
+        player.pauseVideo();
+    }
+    else {
+        player.playVideo();
+    }
+
+    updateButton();
+}
+
+function updateButton() {
+    const i = playButton.querySelector('i');
+
+    if (isPlaying) {
+        i.classList.replace('fa-circle-play', 'fa-circle-pause');
+    }
+    else {
+        i.classList.replace('fa-circle-pause', 'fa-circle-play');
+    }
+}
+
+function switchLeftView(e) {
+    if (e.target === songCover && songCover.classList.contains('closed')) {
+        songCover.classList.replace('closed', 'opened');
+        songVideo.classList.replace('opened', 'closed');
+    }
+    else if (e.target === songVideo && songVideo.classList.contains('closed')) {
+        songVideo.classList.replace('closed', 'opened');
+        songCover.classList.replace('opened', 'closed');
+    }
+}
+//#endregion
